c9e76d97d7e331776834c136178f692d86235b3c
ndparker <ndparker@users.noreply.github.com>
2014-10-30 16:17:22 +0100

    Pre-release: version update

--- wtf/__init__.py
+++ wtf/__init__.py
@@ -24,7 +24,7 @@ WSGI Tackling Framework (WTF).
 __author__ = u"Andr\xe9 Malo"
 __docformat__ = "restructuredtext en"
 __license__ = "Apache License, Version 2.0"
-__version__ = ('0.8.20', True, 4266)
+__version__ = ('0.8.21', False, 4267)
 
 
 class Error(Exception):
